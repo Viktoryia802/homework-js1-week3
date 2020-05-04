973fb9eeb31faf417dbfcfd0327a7715e40cd5e6
Viktoryia802 <villette@list.ru>
2020-05-04 10:49:56 +0200

    ex 3 done

--- 3-max.js
+++ 3-max.js
@@ -3,12 +3,20 @@ Max
 Given an array of numbers, create a function to return the maximum 
 */
 
-function max(numbers){
-    //your code here
+function max(numbers) {
+  //your code here
+  var maximum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maximum) {
+      maximum = numbers[i];
+    }
+  }
+
+  return maximum;
 }
 
-const numbersArray1 = [1,3,5,9];
-const numbersArray2 = [7,8,5,7,3,6];
+const numbersArray1 = [1, 3, 5, 9];
+const numbersArray2 = [7, 8, 5, 7, 3, 6];
 
 console.log("Maximum number in first array is " + max(numbersArray1));
 console.log("Maximum number in second array is " + max(numbersArray2));
